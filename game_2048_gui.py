539cd98b5d30867a96f19b98d0ffd412ce0d7582
jarrod <ghbxl@sina.com>
2018-01-03 00:00:02 +0800

    changed layout

--- game_2048_gui.py
+++ game_2048_gui.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from game_2048 import *
-import copy as cp
 import random
 import sys
+import sqlite3
 
+# 借用了yangankang（https://github.com/yangankang/examples/tree/master/2048）的代码，按需求改动了一下
 
 class Game2048Main(QMainWindow):
     def __init__(self):
@@ -14,35 +14,38 @@ class Game2048Main(QMainWindow):
         self.InitUI()
 
     def InitUI(self):
+        self.game = Game2048(self)
+        self.setCentralWidget(self.game)
+
         menubar = self.menuBar()
         exitAction = QAction('&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(self.close)
 
-        recordsAction = QAction('&Records', self)
+        recordsAction = QAction('&Replay', self)
         recordsAction.setShortcut('Ctrl+R')
-        recordsAction.setStatusTip('Show your records')
-        recordsAction.triggered.connect(self.close)
+        recordsAction.setStatusTip('Replay')
+        recordsAction.triggered.connect(self.game.bottom.game_initializer)
 
         fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(recordsAction)
         fileMenu.addAction(exitAction)
+
+        menubar.addAction(recordsAction)
 
         start_number_menu = QMenu('StartNumber', self)
         sn0 = QAction('original', self)
-        sn1024 = QAction('1024', self)
-        sn2048 = QAction('2048', self)
-        sn4096 = QAction('4096', self)
-        sn8192 = QAction('8192', self)
-        start_number_menu.addAction(sn1024)
-        start_number_menu.addAction(sn2048)
-        start_number_menu.addAction(sn4096)
-        start_number_menu.addAction(sn8192)
+        sn0.triggered.connect(self.set_start_number)
+        start_number_menu.addAction(sn0)
+        for i in range(10, 14):
+            sntmp = QAction(str(2**i), self)
+            sntmp.triggered.connect(self.set_start_number)
+            start_number_menu.addAction(sntmp)
 
         level_menu = QMenu('Level', self)
         for i in range(1,7):
             level_tmp = QAction(str(i), self)
+            level_tmp.triggered.connect(self.set_level)
             level_menu.addAction(level_tmp)
 
         settingMenu = menubar.addMenu('&Settings')
@@ -50,10 +53,17 @@ class Game2048Main(QMainWindow):
         settingMenu.addMenu(level_menu)
         menubar.setNativeMenuBar(False)
 
-        game = Game2048(self)
-        self.setCentralWidget(game)
         self.center()
         self.setFixedSize(540, 600)
+
+        # 获取一下第一级的记录
+        records = read_db()
+        if records and 0 in records.keys():
+            record = records[0]
+        else:
+            record = 0
+        self.game.set_record(record)
+
         self.show()
 
     def center(self):
@@ -62,36 +72,53 @@ class Game2048Main(QMainWindow):
         self.move(int((screen.width()-size.width())/2),
             int((screen.height()-size.height())/2))
 
+    def set_level(self):
+        sender = self.sender()
+        level = int(sender.text()) - 1
+        self.game.bottom.level = level
+        self.game.bottom.game_initializer()
+        self.game.level_lbl.setText('当前级别：%s级'%str(level+1))
+
+        # 获取这一级别的record
+        records = read_db()
+        if records and level in records.keys():
+            record = records[level]
+        else:
+            record = 0
+        self.game.set_record(record)
+
+
+    def set_start_number(self):
+        sender = self.sender()
+        start_number = int(sender.text())
+        self.game.bottom.start_number = start_number
+        self.game.bottom.game_initializer()
 
 
 class Game2048(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.score = 0
-        self.dead = False
-        self.numbers = []
-        self.level = 0
-        self.start_number = 0
         self.initUI()
         self.parent = parent
 
     def initUI(self):
-        hbox = QHBoxLayout(self)
-        vbox = QVBoxLayout(self)
+        self.level_lbl = QLabel('当前级别：1级', self)
+        self.level_lbl.move(30, 5)
+        self.level_lbl.setFixedSize(120, 50)
 
-        self.topleft = QLabel('当前分数：', self)
+        self.topleft = QLabel('当前分数：0', self)
         # self.topleft.setFrameShape(QFrame.StyledPanel)
-        self.topleft.move(30, 5)
-        self.topleft.setFixedSize(210, 50)
+        self.topleft.move(210, 5)
+        self.topleft.setFixedSize(120, 50)
 
         self.topright = QLabel('最高分数：20000', self)
         # self.topright.setFrameShape(QFrame.StyledPanel)
-        self.topright.setFixedSize(210, 50)
-        self.topright.move(300, 5)
+        self.topright.setFixedSize(120, 50)
+        self.topright.move(390, 5)
 
         self.bottom = GameCanvas(self)
         self.bottom.setFixedSize(500, 500)
-        self.bottom.move(20, 80)
+        self.bottom.move(20, 60)
 
         self.setWindowTitle("2048")
         self.resize(540, 600)
@@ -107,6 +134,13 @@ class Game2048(QWidget):
     def keyPressEvent(self, QKeyEvent):
         self.bottom.keyPressEvent(QKeyEvent)
 
+    def set_score(self, score):
+        self.topleft.setText('当前分数：' + str(score))
+
+    def set_record(self, record):
+        self.topright.setText('最高分数：' + str(record))
+
+
 
 class GameCanvas(QLabel):
     # 只读的背景16个方格
@@ -114,9 +148,10 @@ class GameCanvas(QLabel):
     # 背景16个方格的xy位置也是只读的
     item_bg_pos = []
     item_data = []
+    score = 0
 
     parent_x = 20
-    parent_y = 80
+    parent_y = 60
     split_width = 16
     rect_width = 105
 
@@ -127,9 +162,12 @@ class GameCanvas(QLabel):
         # self.resize(500, 500)
         self.move(self.parent_x, self.parent_y)
         self.setStyleSheet("QLabel{background-color:#bbada0;color:#bbada0;border-radius:6}")
-
         self.setFocusPolicy(Qt.StrongFocus)
-        self.set_item_bg()
+
+        self.level = 0
+        self.start_number = 0
+
+        # self.set_item_bg()
         self.set_init_rect()
 
     def set_init_rect(self):
@@ -163,11 +201,22 @@ class GameCanvas(QLabel):
                     zero_ds.append({"i": i, "j": j})
 
         if len(zero_ds) > 0:
-            rnd = int(random.uniform(0, len(zero_ds)))
-            k = int(random.uniform(0, 10))
-            number = 2
-            if k == 2 or k == 10:
-                number = 4
+            # level 决定每次随机出现的数字的概率以及最大的数字是多少
+            # level 0: 随机出现2和4，4的概率是20%
+            # level 1：随机出现2、4、8，8的概率是5%
+            # level 2：随机出现2、4、8、16，8和16的概率分别是5%
+            # level 3: 随机出现2、4、8、16、32
+            level = self.level
+            tmp = random.randint(1, 100)
+            if tmp <= 100-level*5:
+                k = random.randint(0, 9)
+                number = 2
+                if k <= 1:
+                    number = 4
+            else:
+                number = 2 ** (int((100-tmp)/5) + 3)
+
+            rnd = random.randint(0, len(zero_ds)-1)
             d = zero_ds[rnd]
             d["num"] = number
             d["x"] = self.item_bg_pos[d["i"]][d["j"]]["x"]
@@ -184,16 +233,32 @@ class GameCanvas(QLabel):
         return None
 
     def game_initializer(self):
-        level = self.parent.level
-        start_number = self.parent.start_number
+        # 所有初始化为0
+        self.score = 0
+        self.parent.set_score(self.score)
+
+        # 所有item重置，画面重置
+        items = []
+        if self.item_data:
+            for i in range(0, 4):
+                for j in range(0, 4):
+                    item = self.item_data[i][j]["Item"]
+                    if item:
+                        items.append(item)
+            self.item_data = []
+        self.set_item_bg()
+        self.redraw_rect(items)
+
+        # 选择初始化数字
+        start_number = self.start_number
         indexes = []
         for i in range(4):
             for j in range(4):
                 indexes.append({'i': i, 'j': j})
-        d = random.sample(indexes, 1)
+        d = random.sample(indexes, 1)[0]
         # 根据初始数字和级别随机生成2个数字
         if start_number:
-            d = {'num': start_number}
+            d['num'] = start_number
             d["x"] = self.item_bg_pos[d["i"]][d["j"]]["x"]
             d["y"] = self.item_bg_pos[d["i"]][d["j"]]["y"]
 
@@ -227,24 +292,39 @@ class GameCanvas(QLabel):
         calculate = GameCalculate(self.item_data)
         calculate.calculate(direction)
         self.item_data = calculate.get_data()
-
-        # for d in self.item_data:
-        #     print(d)
-        # for p in self.item_bg_pos:
-        #     print(p)
+        self.score += calculate.get_score()
+        self.parent.set_score(self.score)
 
         self.redraw_rect(items)
         # self.check_is_win()
-        # dead = self.check_is_dead()
-        dead = False
+        dead = self.check_is_dead()
         if dead:
+            # 更新记录
+            # 获取这一级别的record
+            records = read_db()
+            if records:
+                if self.level in records.keys():
+                    if self.score > records[self.level]:
+                        write_db(self.level, self.score, update_type='update')
+                        self.parent.set_record(self.score)
+                    else:
+                        pass
+                else:
+                    write_db(self.level, self.score)
+                    self.parent.set_record(self.score)
+            else:
+                write_db(self.level, self.score)
+                self.parent.set_record(self.score)
+
             reply = QMessageBox.question(self.parent, '提示信息',
                                          "您已经输了，是否重新开始?", QMessageBox.Yes, QMessageBox.No)
-            # if reply == QMessageBox.Yes:
-            #     print("OK")
-            # else:
-            #     print("NO")
+            if reply == QMessageBox.Yes:
+                self.game_initializer()
+                # print("OK")
+            else:
+                print("NO")
         self.random_rect_item()
+
 
     def redraw_rect(self, items):
         for i in range(0, 4):
@@ -277,13 +357,23 @@ class GameCanvas(QLabel):
 
     def check_is_dead(self):
         dead = True
-        for i in range(1, 5):
-            tmp_data = cp.deepcopy(self.item_data)
-            calculate = GameCalculate(tmp_data)
-            calculate.calculate(direction=i)
-            dead = calculate.get_dead_status()
-            if not dead:
-                break
+        #先拷贝一遍item里面的数字，
+        tmp_numbers = []
+        for i in range(4):
+            tmp_line = []
+            for j in range(4):
+                tmp_line.append(self.item_data[i][j]["Number"])
+            if 0 in tmp_line:
+                return False
+            else:
+                tmp_numbers.append(tmp_line)
+
+        for i in range(4):
+            for j in range(3):
+                if tmp_numbers[i][j] == tmp_numbers[i][j+1]:
+                    return False
+                if tmp_numbers[j][i] == tmp_numbers[j+1][i]:
+                    return False
         return dead
 
 
@@ -351,9 +441,10 @@ class NumberRect(QLabel):
 class GameCalculate():
     def __init__(self, item_data):
         self.data = item_data
-        self.dead = True
+        self.score = 0
 
     def calculate(self, direction):
+        self.score = 0
         if len(self.data) <= 0:
             return 0
         if direction == 1:
@@ -379,7 +470,7 @@ class GameCalculate():
                         self.data[k["j"]][k["i"]]["Number"] = k_v["Number"] + l["Number"]
                         self.data[j][i] = {"Item": None, "Number": 0}
                         k = None
-                        self.dead = False
+                        self.score += 2 * l["Number"]
                     else:
                         if l["Number"] != 0: k = {"i": i, "j": j}
                 else:
@@ -393,7 +484,6 @@ class GameCalculate():
                                 swap = self.data[k][i]
                                 self.data[k][i] = self.data[j][i]
                                 self.data[j][i] = swap
-                                self.dead = False
                                 break
 
     def down_run(self):
@@ -407,7 +497,7 @@ class GameCalculate():
                         self.data[k["j"]][k["i"]]["Number"] = k_v["Number"] + l["Number"]
                         self.data[j][i] = {"Item": None, "Number": 0}
                         k = None
-                        self.dead = False
+                        self.score += 2*l["Number"]
                     else:
                         k = {"i": i, "j": j}
                 else:
@@ -422,7 +512,6 @@ class GameCalculate():
                                 swap = self.data[k][i]
                                 self.data[k][i] = self.data[3 - j][i]
                                 self.data[3 - j][i] = swap
-                                self.dead = False
                                 break
 
     def left_run(self):
@@ -436,7 +525,7 @@ class GameCalculate():
                         self.data[k["i"]][k["j"]]["Number"] = k_v["Number"] + l["Number"]
                         self.data[i][j] = {"Item": None, "Number": 0}
                         k = None
-                        self.dead = False
+                        self.score += 2 * l["Number"]
                     else:
                         k = {"i": i, "j": j}
                 else:
@@ -450,7 +539,6 @@ class GameCalculate():
                                 swap = self.data[i][k]
                                 self.data[i][k] = self.data[i][j]
                                 self.data[i][j] = swap
-                                self.dead = False
                                 break
 
     def right_run(self):
@@ -464,7 +552,7 @@ class GameCalculate():
                         self.data[k["i"]][k["j"]]["Number"] = k_v["Number"] + l["Number"]
                         self.data[i][j] = {"Item": None, "Number": 0}
                         k = None
-                        self.dead = False
+                        self.score += 2 * l["Number"]
                     else:
                         k = {"i": i, "j": j}
                 else:
@@ -478,11 +566,37 @@ class GameCalculate():
                                 swap = self.data[i][k]
                                 self.data[i][k] = self.data[i][3 - j]
                                 self.data[i][3 - j] = swap
-                                self.dead = False
                                 break
 
-    def get_dead_status(self):
-        return self.dead
+    def get_score(self):
+        return self.score
+
+def read_db(name='jarrod'):
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    cursor.execute('select * from game_2048 where name=?', (name, ))
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    if not result:
+        return {}
+    records = {}
+    for record in result:
+        level = record[2]
+        score = record[3]
+        records[level] = score
+    return records
+
+def write_db(level, score, name='jarrod', update_type='insert'):
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    if update_type == 'insert':
+        cursor.execute('insert into game_2048 (name, level, score) VALUES ("%s", %d, %d)'%(name, level, score))
+    else:
+        cursor.execute('update game_2048 set score=? where (name=? and level=?)', (score, name, level, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 if __name__ == '__main__':
